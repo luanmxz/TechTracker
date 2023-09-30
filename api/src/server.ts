e8a392b015f41a93ff018352e95226a4faee928b
luanmxz <luanmarcenemxz22@gmail.com>
2023-09-30 19:14:03 -0300

    First configs

---
+++ api/src/server.ts
@@ -0,0 +1,37 @@
+import Fastify from 'fastify';
+import workspacesMethods from './workspaces';
+import { IWorkspace } from './IWorskpace';
+
+const fastify = Fastify({
+	logger: true,
+});
+
+fastify.get('/api/getWorkspaces', async (req, res) => {
+	try {
+		const workspaces = await workspacesMethods.getWorkspaces();
+
+		console.log(workspaces[0].name);
+
+		res.send(workspaces);
+	} catch (error: any) {
+		res.status(500).send({ error: error.message });
+	}
+});
+
+fastify.post('/api/createWorkspace', async (req, res) => {
+	try {
+		const reqBody: IWorkspace = req.body as IWorkspace;
+		const workspace = await workspacesMethods.createWorkspace(reqBody);
+
+		res.status(201).send({ message: 'Workspace criado com sucesso!' })
+	} catch (error: any) {
+		res.status(500).send({ error: error.message });
+	}
+});
+
+fastify.listen({ port: 3000 }, function (err, address) {
+	if (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+});
---
+++ api/src/workspaces.ts
@@ -0,0 +1,33 @@
+import sql from './db.js';
+import { IWorkspace } from './IWorskpace.js';
+
+async function getWorkspaces() {
+    try {
+        const workspaces = await sql`
+			select name from tb01_workspaces
+		`;
+
+        return workspaces;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function createWorkspace(workspace: IWorkspace) {
+    try {
+        await sql`
+            insert into tb01_workspaces (name) values (${workspace.name});
+        `;
+    } catch (error) {
+        throw error;
+    }
+
+}
+
+
+const workspacesMethods = {
+    getWorkspaces,
+    createWorkspace
+};
+
+export default workspacesMethods;
