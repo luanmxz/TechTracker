175bbdcb972b9cb61efc7db85114f3359efb55f9
luanmxz <luanmarcenemxz22@gmail.com>
2023-10-04 21:10:59 -0300

    CHANGE: Removed env var SERVER_PORT

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,7 +5,7 @@ import Fastify from 'fastify';
 import userRoutes from './routes/user-routes';
 import authRoutes from './routes/auth-routes';
 
-const port = parseInt(process.env.SERVER_PORT as string, 10);
+const port = parseInt(process.env.PORT as string, 10);
 
 const fastify = Fastify({
 	logger: true,
@@ -14,7 +14,7 @@ const fastify = Fastify({
 fastify.register(userRoutes);
 fastify.register(authRoutes);
 
-fastify.listen({ port: port }, function (err, address) {
+fastify.listen({ port: port || 3000 }, function (err, address) {
 	if (err) {
 		fastify.log.error(err);
 		process.exit(1);
