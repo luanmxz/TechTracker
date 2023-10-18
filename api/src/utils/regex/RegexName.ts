0e33707f0173082dbf55b2a9bab6cf3f8e9bd9ca
luanmxz <luanmarcenemxz22@gmail.com>
2023-10-18 19:19:23 -0300

    CHANGE: max length of username changed to 20

--- api/src/utils/regex/RegexName.ts
+++ api/src/utils/regex/RegexName.ts
@@ -1,5 +1,5 @@
 const atLeastThreeChar = /^.{3,}$/;
-const exceedNameMaxLenght = /^.{41,}$/;
+const exceedNameMaxLenght = /^.{20,}$/;
 const nameCannotContainEspecialChar = /^[a-zA-Z\s]*$/;
 
 export { atLeastThreeChar, exceedNameMaxLenght, nameCannotContainEspecialChar };
